a6105efdf5192482d3dd4698e683278429402852
David Heinebäck <du222aa@student.lnu.se>
2022-04-08 13:59:08 +0200

    add: Types

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,12 +1,23 @@
 export interface Root {
   id: number
-  Day: Date
-  Countries: Country[]
+  date: string
+  countries: Country[]
 }
 
 export interface Country {
   country: string
+  code: Codes.global | Codes.us | Codes.se | Codes.gb | Codes.br | Codes.au
   toplist: Song[]
+}
+
+enum Codes {
+  global = 'global',
+  se = 'se',
+  us = 'us',
+  gb = 'gb',
+  br = 'br',
+  au ='au'
+  
 }
 
 export interface Song {
