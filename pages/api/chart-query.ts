156ebba34c3cac2230e89dc9d5df22d7e39c8e5a
David Heinebäck <du222aa@student.lnu.se>
2022-04-15 12:21:26 +0200

    add: query to elasticsearch

--- pages/api/chart-query.ts
+++ pages/api/chart-query.ts
@@ -1,24 +1,87 @@
-import { Client } from "@elastic/elasticsearch"
-import fs from 'fs-extra'
-import { NextApiRequest, NextApiResponse } from "next"
+import { Client } from '@elastic/elasticsearch'
+import { NextApiRequest, NextApiResponse } from 'next'
+import elasticClient from '../../components/ElasticClient'
 
-export const client = new Client({
-  node: 'https://localhost:9200',
-  auth: {
-    username: process.env.ELASTIC_USERNAME!,
-    password: process.env.ELASTIC_PASSWORD!
+const searchOptions = {
+  index: 'scrapedspotifydata',
+  body: {
+    aggs: {
+      '0': {
+        terms: {
+          field: 'country.keyword',
+          order: {
+            '2': 'desc',
+          },
+          size: 7,
+        },
+        aggs: {
+          '1': {
+            date_histogram: {
+              field: 'date',
+              fixed_interval: '3h',
+              time_zone: 'Europe/Stockholm',
+            },
+            aggs: {
+              '2': {
+                sum: {
+                  field: 'toplist.streams',
+                },
+              },
+            },
+          },
+          '2': {
+            sum: {
+              field: 'toplist.streams',
+            },
+          },
+        },
+      },
+    },
+    size: 0,
+    fields: [
+      {
+        field: 'date',
+        format: 'date_time',
+      },
+    ],
+    script_fields: {},
+    stored_fields: ['*'],
+    runtime_mappings: {},
+    _source: {
+      excludes: [],
+    },
+    query: {
+      bool: {
+        must: [],
+        filter: [
+          {
+            range: {
+              date: {
+                format: 'strict_date_optional_time',
+                gte: '2022-04-01T07:22:54.890Z',
+                lte: '2022-04-06T07:38:53.127Z',
+              },
+            },
+          },
+        ],
+        should: [],
+        must_not: [],
+      },
+    },
   },
-  tls: {
-    ca: fs.readFileSync(process.env.CERT_PATH!),
-    rejectUnauthorized: false
-  }
-})
-
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  console.log('I RÄTT API ROUTE')
+  if (req.method !== 'GET') {
+    res.setHeader('Allow', ['GET'])
+    return res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
 
-  res.status(200).json({ data: 'hej' })
+  const response = await elasticClient.search(searchOptions)
+
+  res.status(200).json({ aggregations: response.aggregations })
 }
